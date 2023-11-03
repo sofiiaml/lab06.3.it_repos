07430cb0caefa6bdcb55ccd576d0c9cc0b92a13d
sofiiaml <sofiia.molotiuk.ik.2023@lpnu.ua>
2023-11-03 11:51:04 +0200

    pr6.3.it

---
+++ PR6.3.it/PR6.3.it.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+void Input(double* a, const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << "a[" << i << "] = ";
+		cin >> a[i];
+	}
+	cout << endl;
+}
+
+int main()
+{
+	int n;
+	cout << "n="; cin >> n;
+	double* a = new double[n];
+	Input(a, n);
+	cout << "Array: ";
+	for (int i = 0; i < n; i++) {
+		cout << a[i] << " ";
+	}
+	cout << endl;
+	delete[] a;
+
+	return 0;
+}
